ba7cecce1c07c972d7c1a0b085370722100f28b3
candrisevic <candrisevic@gmail.com>
2014-11-25 20:30:45 -0500

    initial commit

---
+++ zombies.js
@@ -0,0 +1,14 @@
+var user = prompt("Zombies have taken over... Which scenario would you like to perform? CITY, SUBURB, or FOREST").toUpperCase();
+
+switch (user) {
+	case 'CITY':
+	break;
+
+	case 'SUBURB':
+	break;
+
+	case 'FOREST':
+	break;
+
+	default:
+}
