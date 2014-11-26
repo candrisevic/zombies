a9825b353b89b19ecdc77ea3e5dca02578444178
candrisevic <candrisevic@gmail.com>
2014-11-25 21:16:44 -0500

    Added to city option, areas available

--- zombies.js
+++ zombies.js
@@ -1,7 +1,21 @@
-var user = prompt("Zombies have taken over... Which scenario would you like to perform? CITY, SUBURB, or FOREST").toUpperCase();
+// First prompt for user
+var user = prompt("Zombies are taking over... Which scenario would you like to perform? CITY, SUBURB, or FOREST.").toUpperCase();
 
 switch (user) {
 	case 'CITY':
+		var city = prompt("You have chose to begin in a city... Do you want TORONTO, NEW YORK, TOKYO or do you want to LEAVE?").toUpperCase();
+		if (city === 'TORONTO') {
+			alert("You find yourself in Toronto, you are downtown and live on the top floor of a house.");
+		}
+		else if (city === "NEW YORK") {
+			alert("You find yourself in New York, you are downtown and live on the 15th floor of an upscale apartment.");
+		}
+		else if (city === "TOKYO") {
+			alert("You find yourself in Tokyo, you live in a small apartment with your dog.");
+		}
+		else {
+			alert("You entered an incorrect option, please try again.");
+		}
 	break;
 
 	case 'SUBURB':
